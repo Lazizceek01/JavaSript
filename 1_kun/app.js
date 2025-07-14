8eecd790a3644dfea75b6906fa5f663f31ae1095
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-14 11:40:40 +0500

    1_kun

---
+++ 1_kun/app.js
@@ -0,0 +1,74 @@
+// JavaScript - Ozi nima ? 
+// Bu bu vebsahifalarni bez uchun  yordam beradigon til.
+// Exampel 
+// Tugmalarni bosganda nimadur chiqarish
+// Animatsiyalar 
+// Formalarni toldirishda tekshiruv
+// Oyinlar ilovalar real vaqtli chatlar 
+
+
+
+// History
+// 1) Funder: Brender Each 
+// Dastlablki nomi "LiveScript" edi 
+// keyin esa "JavaScreptga" ozgartirilgan
+
+
+// Imloniyatlari
+// Interaktiv saytlar yaratish 
+// Dom bilan ishlash (Veb sahifalardaigi elmentlarni ozgartirish)
+// Mobil ilovalar va oyinlar 
+// Server dastrulari (Node.js)
+
+
+
+console.log("Hello world");
+console.log("Welmo to Uzbekistan !");
+
+
+// Asosan nimalar va qayerda ishlatiladi ? 
+// Veb sahifalarda, mslmkjnsbvdobil desktop dailovalarda serlada ishlash 
+
+console.log("Welcome to Uzbekistan !");
+
+
+// Task 1kun
+// Task 1 
+console.log("Men JavaScript organishni boshladim");
+console.log("Bugun birinchi 1kun");
+console.log("Keling kod yozamiz!");
+
+// Task 2 
+console.log(10 + 5);
+console.log(20 - 7);
+console.log(3 * 4);
+console.log(16 / 2);
+
+// Task 3
+console.log("Ismim: Laziz !");
+console.log("Yoshim 20 da");
+console.log("Kasbim: Dasturchi bolishni hohlayman !");
+
+
+// Task 4 
+console.log("JavaScrept bu juda ajoyip til");
+console.log("Keling oldinga intilaylik !");
+
+
+
+
+// Proceccing 
+// Task 1
+console.log("Yoshim: 20 yosh");
+
+
+// Task 
+console.log(10 + 5);
+console.log(20 - 7);
+console.log(3 * 4);
+console.log(16 / 2);
+
+
+// task 3 
+console.log("Men ")
+
