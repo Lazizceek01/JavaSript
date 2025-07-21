02cf73bc7b72ef78234539119f002d5d28d77940
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-21 22:50:26 +0500

    6kun

---
+++ 6kun/app.js
@@ -0,0 +1,104 @@
+// Date: 21.07.2025.
+// Topic: 7kun ummumiy takrorlash 
+// All world and tasks or lesson 
+
+// Amaliy mashq boolean uchun 
+// let yosh = prompt("Yoshingizni kiriting:");
+// let katta = yosh >= 18;
+// console.log("Siz katta odammi?", katta);
+
+// let yosh = prompt("Yoshingizni kiriting:");
+// yosh = Number(yosh);
+
+// if (yosh < 18) {
+//   console.log("Yosh bolla lekin orzulari katta:")
+// } else if (yosh >= 18) {
+//   console.log("Katta odam katta imkoniyatlar!");
+// } else {
+//   console.log("Sizning kealjagingiz hali oldinda !")
+// }
+
+
+
+// Repeat 
+// let yosh = prompt("Yoshingizni kiriting:");
+// yosh = Number(yosh);
+
+// if (yosh < 18) {
+//   console.log("Yoshbola lekin orzulari katta !")
+// } else if (yosh >= 18) {
+//   console.log(" Katta odam katta ")
+// }
+
+
+// Again 
+
+// let yosh = prompt("Yoshingizni kiriting:");
+// yosh = Number(yosh);
+
+// if (yosh < 18) {
+//   console.log("Yoshbola lekin katta orzular bilan !")
+// } else if(yosh >= 18){
+//   console.log("Katta odam katta imloniyatlar ! ")
+// } else {
+//   console.log("Sizning kelajagingiz hali oldinda !")
+// }
+
+
+
+
+
+// Task 2 
+// ikkita sonni bir biriga solishtirish 
+// let a = prompt("1 - son:");
+// let b = prompt("2 - son:");
+
+// if (a > b) {
+//   console.log(a + " - Katta");
+// } else if (a < b) {
+//   console.log(b + " - Katta"); 
+// } else {
+//   console.log("Ikkalasi teng !");
+// }
+
+
+// Repeat 
+// let a = prompt("1 - son: ");
+// let b = prompt("2 - son: ");
+
+// if (a > b) {
+//   console.log(a + " - Katta !");
+// } else if (a < b) {
+//   console.log(b + " - Katta !");
+// } else {
+//   console.log("Ikkalsi teng !");
+// }
+
+
+
+// Again 
+
+// let a = prompt(" 1 - son: ");
+// let b = prompt(" 2 - son: ");
+
+// if (a > b) {
+//   console.log(a + " -Katta !");
+// } else if (a < b) {
+//   console.log(b + " - Katta !")
+// } else {
+//   console.log("Ikkalasi teng ! ");
+// }
+
+
+
+
+// Mini projcet in  JavaScript 
+let son1 = prompt("Birinchi soni kiriting: ");
+let amal = prompt("Amalni tanlang ( +,   -,  /, * ): ");
+let son2 = prompt("Ikkinchi soni kiriting: ")
+
+if (amal === "+") {
+  natija = son1 + son2;
+} else if (amal === "-") {
+  natija 
+} 
