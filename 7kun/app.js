b10e98aaae8448092203c7541537c4e3e654301f
Laziz Azizov <azizovlaziz33@gmail.com>
2025-07-21 22:50:45 +0500

    7kun

---
+++ 7kun/app.js
@@ -0,0 +1,150 @@
+// Date: 21.07.2025.
+// Topic: 7kun ummumiy takrorlash 
+// All world and tasks or lesson 
+
+// Amaliy mashq boolean uchun 
+// let yosh = prompt("Yoshingizni kiriting:");
+// let katta = yosh >= 18;
+// console.log("Siz katta odammi?", katta);
+
+// let yosh = prompt("Yoshingizni kiriting:");
+// yosh = Number(yosh);
+
+// if (yosh < 18) {
+//   console.log("Yosh bolla lekin orzulari katta:")
+// } else if (yosh >= 18) {
+//   console.log("Katta odam katta imkoniyatlar!");
+// } else {
+//   console.log("Sizning kealjagingiz hali oldinda !")
+// }
+
+
+
+// Repeat 
+// let yosh = prompt("Yoshingizni kiriting:");
+// yosh = Number(yosh);
+
+// if (yosh < 18) {
+//   console.log("Yoshbola lekin orzulari katta !")
+// } else if (yosh >= 18) {
+//   console.log(" Katta odam katta ")
+// }
+
+
+// Again 
+
+// let yosh = prompt("Yoshingizni kiriting:");
+// yosh = Number(yosh);
+
+// if (yosh < 18) {
+//   console.log("Yoshbola lekin katta orzular bilan !")
+// } else if(yosh >= 18){
+//   console.log("Katta odam katta imloniyatlar ! ")
+// } else {
+//   console.log("Sizning kelajagingiz hali oldinda !")
+// }
+
+
+
+
+
+// Task 2 
+// ikkita sonni bir biriga solishtirish 
+// let a = prompt("1 - son:");
+// let b = prompt("2 - son:");
+
+// if (a > b) {
+//   console.log(a + " - Katta");
+// } else if (a < b) {
+//   console.log(b + " - Katta"); 
+// } else {
+//   console.log("Ikkalasi teng !");
+// }
+
+
+// Repeat 
+// let a = prompt("1 - son: ");
+// let b = prompt("2 - son: ");
+
+// if (a > b) {
+//   console.log(a + " - Katta !");
+// } else if (a < b) {
+//   console.log(b + " - Katta !");
+// } else {
+//   console.log("Ikkalsi teng !");
+// }
+
+
+
+// Again 
+
+// let a = prompt(" 1 - son: ");
+// let b = prompt(" 2 - son: ");
+
+// if (a > b) {
+//   console.log(a + " -Katta !");
+// } else if (a < b) {
+//   console.log(b + " - Katta !")
+// } else {
+//   console.log("Ikkalasi teng ! ");
+// }
+
+
+
+
+// Mini projcet in  JavaScript 
+// let son1 = prompt("Birinchi soni kiriting: ");
+// let amal = prompt("Amalni tanlang ( +,   -,  /, * ): ");
+// let son2 = prompt("Ikkinchi soni kiriting: ")
+
+// if (amal === "+") {
+//   natija = son1 + son2;
+// } else if (amal === "-") {
+//   natija = son1 - son2;
+// } else if (amal === "*") {
+//   natija = son1 * son2;
+// } else if (amal === "/") {
+//   natija = son1 / son2;
+// } else {
+//   console.log(" Notogti amal kiritildi!")
+// }
+
+// console.log("Natija:", natija);
+
+
+
+
+
+// 2 ta sonni bir biriga taqoslash 
+// let a = prompt("1 - son:");
+// let b = prompt("2 - son:");
+
+// if (a > b) {
+//   console.log(a + "Katta");
+// } else if (a < b) {
+//   console.log(b + "Katta");
+// } else {
+//   console.log("Ikkala son teng !")
+// };
+
+
+
+
+// Project mini calc 
+// let son1 = prompt("Son1: ");
+// let amal = prompt("Amal ( + , - , / , *) ");
+// let son2 = prompt("Son2: ");
+
+// if (amal === "+") {
+//   natija = son1 + son2;
+// } else if (amal === "-") {
+//   natija = son1 - son2;
+// } else if (amal === "*") {
+//   natija = son1 * son2;
+// } else if (amal === "/"){
+//   natija = son1 / son2;
+// } else {
+//   console.log("Notogri amaliyot kiritilgan !")
+// }
+
+// console.log("Natija:", natija);
